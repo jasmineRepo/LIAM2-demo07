fe4a26c6501365ebe9dd4d668c1c7fca90db04c9
rer98 <r.richardson05@alumni.imperial.ac.uk>
2015-09-23 17:08:07 +0100

    Renamed Foglio1 to Sheet1 in excel input files and in Parameters.java

--- demo07/src/it/unito/jas2/demo07/data/Parameters.java
+++ demo07/src/it/unito/jas2/demo07/data/Parameters.java
@@ -31,17 +31,17 @@ public class Parameters {
 	public static void loadParameters() {
 		
 		// probabilities
-		pBirth = ExcelAssistant.loadCoefficientMap("input/p_birth.xls", "Foglio1", 1, 59);
-		pDeathM = ExcelAssistant.loadCoefficientMap("input/p_death_m.xls", "Foglio1", 1, 59);
-		pDeathF = ExcelAssistant.loadCoefficientMap("input/p_death_f.xls", "Foglio1", 1, 59);
-		pMarriage = ExcelAssistant.loadCoefficientMap("input/p_marriage.xls", "Foglio1", 3, 4);
-		pDivorce = ExcelAssistant.loadCoefficientMap("input/p_divorce.xls", "Foglio1", 2, 59);
-		pInWork = ExcelAssistant.loadCoefficientMap("input/p_inwork.xls", "Foglio1", 3, 59);
+		pBirth = ExcelAssistant.loadCoefficientMap("input/p_birth.xls", "Sheet1", 1, 59);
+		pDeathM = ExcelAssistant.loadCoefficientMap("input/p_death_m.xls", "Sheet1", 1, 59);
+		pDeathF = ExcelAssistant.loadCoefficientMap("input/p_death_f.xls", "Sheet1", 1, 59);
+		pMarriage = ExcelAssistant.loadCoefficientMap("input/p_marriage.xls", "Sheet1", 3, 4);
+		pDivorce = ExcelAssistant.loadCoefficientMap("input/p_divorce.xls", "Sheet1", 2, 59);
+		pInWork = ExcelAssistant.loadCoefficientMap("input/p_inwork.xls", "Sheet1", 3, 59);
 		
 		// regression coefficients
-		coeffMarriageFit = ExcelAssistant.loadCoefficientMap("input/reg_marriage.xls", "Foglio1", 1, 1);
-		coeffDivorce = ExcelAssistant.loadCoefficientMap("input/reg_divorce.xls", "Foglio1", 1, 1);
-		coeffInWork = ExcelAssistant.loadCoefficientMap("input/reg_inwork.xls", "Foglio1", 3, 1);
+		coeffMarriageFit = ExcelAssistant.loadCoefficientMap("input/reg_marriage.xls", "Sheet1", 1, 1);
+		coeffDivorce = ExcelAssistant.loadCoefficientMap("input/reg_divorce.xls", "Sheet1", 1, 1);
+		coeffInWork = ExcelAssistant.loadCoefficientMap("input/reg_inwork.xls", "Sheet1", 3, 1);
 		
 		// definition of regression models		
 		regMarriageFit = new LinearRegression(coeffMarriageFit);
